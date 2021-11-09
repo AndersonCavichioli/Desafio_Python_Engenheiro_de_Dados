f6daf4570e18a13511bc0a68a9104f64931c4341
Anderson Cavichioli Valerio <p22.anderson@webmotors.local>
2021-11-09 11:17:13 -0300

    subindo os arquivos

---
+++ test1.py
@@ -0,0 +1,22 @@
+import os
+
+path = "C:\\Users\\p22.ribeiro\\OneDrive\\repocode\\challenge-python\\dataset"
+dirs = os.listdir(path)
+
+files = len(dirs)
+# print(files)
+
+i = 0
+documentos = dict()
+for i in files:
+  for filename in dirs:
+    if filename.endswith(""):
+      documentos = 
+      print(documentos)
+      
+
+"""
+# Exibição do dicionário
+for documentoID, filename in sorted(test.items()):
+  print(documentoID, filename)
+"""
